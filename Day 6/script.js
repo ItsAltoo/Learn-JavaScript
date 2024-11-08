3963bd36080d7c8705fb4219756dde7b518c5580
ItsAltoo <Itsmalik57@gmail.com>
2024-11-08 21:01:19 +0800

    8.11.24

---
+++ Day 6/script.js
@@ -0,0 +1,11 @@
+/* 
+1. Tugas: Kalkulator Sederhana
+Deskripsi: Buat aplikasi kalkulator sederhana yang dapat melakukan operasi penjumlahan, pengurangan, perkalian, dan pembagian.
+
+Tujuan: Latihan manipulasi DOM, event handling, dan dasar logika JavaScript.
+Fitur Utama:
+Input untuk dua angka.
+Tombol untuk memilih operasi (+, -, *, /).
+Menampilkan hasil perhitungan.
+Tambahan CSS: Desain tombol dan input agar terlihat menarik.
+*/
